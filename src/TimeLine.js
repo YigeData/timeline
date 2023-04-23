357b15f807867f4976cc7c6a11998710735dca7a
hanjinfeng <hanjinfeng@hightopo.com>
2023-04-23 11:21:10 +0800

    插件重构

--- src/TimeLine.js
+++ src/TimeLine.js
@@ -5,45 +5,228 @@ ui.TimeLine = function () {
     ht.ui.TimeLine.superClass.constructor.call(this);
 };
 
-def('ht.ui.TimeLine', ht.ui.VBoxLayout, {
+def('ht.ui.TimeLine', ht.ui.ViewGroup, {
 
     // 样式属性
-    ui_ac: ['lineColor', 'lineWidth', 'labelFont', 'labelColor', 'labelBackground',
-            'iconBackground', 'headerFont', 'headerColor', 'contentFont', 'contentColor', 'eventBorder', 'eventBackground'],
+    ui_ac: [
+        'lineColor', 'lineWidth', 'labelFont', 'labelColor', 'labelBackground',
+        'iconBackground', 'headerFont', 'headerColor', 'contentFont', 'contentColor', 
+        'eventBorder', 'eventBackground', 'icon', 'iconWidth', 'iconHeight', 'gap',
+        'iconContentGap', 'placement', 'is:center'
+    ],
 
-    __lineColor: '#eeeeee', // 默认时间轴线颜色
-    __lineWidth: 3, // 默认时间轴线宽度
+    __lineColor: 'rgb(228, 231, 237)', // 默认时间轴线颜色
+    __lineWidth: 1, // 默认时间轴线宽度
     __labelFont: '16px Arial', // 默认时间点字体
     __labelColor: 'rgb(255, 255, 255)', // 默认时间点颜色
     __labelBackground: ['ht.ui.drawable.ColorDrawable', '#FEB64D', 5], // 默认时间点背景
     __iconBackground: '#eeeeee', // 默认事件图标背景色
-    __headerFont: '14px Arial', // 默认事件头字体
-    __headerColor: '#000', // 默认事件头颜色
-    __contentFont: '12px Arial', // 默认事件内容字体
-    __contentColor: '#000', // 默认事件内容颜色
-    __padding: 20,
+    __headerFont: '12px arial,sans-serif', // 默认事件头字体
+    __headerColor: '#909399', // 默认事件头颜色
+    __contentFont: '14px arial,sans-serif', // 默认事件内容字体
+    __contentColor: '#303133', // 默认事件内容颜色
+    __icon: 'ht_ui_timeline_icon', // 默认图标
+    __iconWidth: 22, // 默认图标宽度
+    __iconHeight: 22, // 默认图标高度
+    __padding: 10, // 默认组件内边距
+    __gap: 15, // 默认孩子节点间距
+    __iconContentGap: 10, // 默认图标和内容间距
+    __placement: 'top', // 默认 header 在上方
+    __center: false, // 默认 icon 不居中
 
+    /**
+     * @override
+     */
+    figurePreferredSize: function () {
+        var self = this,
+            children = self.getVisibleChildren(),
+            size = {
+                width: self.getPaddingLeft() + self.getPaddingRight() +
+                    self.getBorderLeft() + self.getBorderRight(),
+                height: self.getPaddingTop() + self.getPaddingBottom() +
+                    self.getBorderTop() + self.getBorderBottom()
+            },
+            lineAreaWidth = self._getLineAreaWidth(),
+            width = 0,
+            gap = self.getGap(),
+            iconContentGap = self.getIconContentGap();
+
+        children.each(function(child, index) {
+            var childPreferredSize = child.getPreferredSize(),
+                layoutParams = self.getChildLayoutParams(child) || {},
+                marginLeft = layoutParams.marginLeft || 0,
+                marginRight = layoutParams.marginRight || 0,
+                marginTop = layoutParams.marginTop || 0,
+                marginBottom = layoutParams.marginBottom || 0,
+                type = child.a('$type'),
+                iconHeight,
+                addHeight = childPreferredSize.height + marginTop + marginBottom;
+
+            if (type === 'event') {
+                var info = child.a('$info');
+
+                iconHeight = info.iconHeight || self.getIconHeight();
+            }
+
+            width = Math.max(width, marginLeft + marginRight + childPreferredSize.width + lineAreaWidth + iconContentGap);
+            size.height += iconHeight ? Math.max(iconHeight, addHeight) : addHeight;
+            if (index) size.height += gap;
+        });
+        size.width +=  width;
+
+        return size;
+    },
+
+    /**
+     * @override
+     */
+    getPreferredSizeProperties: function() {
+        var preferredSizeProperties = ht.ui.TimeLine.superClass.getPreferredSizeProperties.call(this);
+
+        preferredSizeProperties = ht.Default.clone(preferredSizeProperties);
+        preferredSizeProperties.iconWidth = true;
+        preferredSizeProperties.iconHeight = true;
+        preferredSizeProperties.gap = true;
+        preferredSizeProperties.iconContentGap = true;
+
+        // 返回 map
+        return preferredSizeProperties;
+    },
     
     /**
-     * 绘制时间轴线
+     * 绘制时间轴线和图标
      * @param x
      * @param y
      * @param width
      * @param height
      * @override
      */
-    validateImpl: function (x, y, width, height) {
+    validateImpl: function (x, y, width, height) {console.log('validate!');
         var self = this;
         ht.ui.TimeLine.superClass.validateImpl.call(self, x, y, width, height);
 
         var g = self.getRootContext(),
-            leftSpacing = self.getPaddingLeft(),
             lineColor = self.getLineColor(),
-            lineWidth = self.getLineWidth();
+            lineWidth = self.getLineWidth(),
+            children = self.getVisibleChildren(),
+            icon = self.getIcon(),
+            iconWidth = self.getIconWidth(),
+            iconHeight = self.getIconHeight(),
+            iconBackground = self.getIconBackground(),
+            lineAreaWidth = self._getLineAreaWidth(),
+            layoutY = self.ty() || 0,
+            scrollWidth = 0,
+            scrollHeight = 0,
+            gap = self.getGap(),
+            iconContentGap = self.getIconContentGap(),
+            center = self.isCenter();
 
+        g.save();
+        g.translate(x, y);
+
+        var funcArr = [];
         g.beginPath();
-        g.fillStyle = lineColor;
-        g.fillRect(x + leftSpacing, y, lineWidth, height);
+        children.each(function(child, index) {
+            var preferredSize = child.getPreferredSize(),
+                layoutParams = self.getChildLayoutParams(child) || {},
+                marginLeft = layoutParams.marginLeft || 0,
+                marginRight = layoutParams.marginRight || 0,
+                marginTop = layoutParams.marginTop || 0,
+                marginBottom = layoutParams.marginBottom || 0,
+                type = child.a('$type'),
+                isEvent = type === 'event',
+                sy = layoutY;
+
+            if (index) sy += gap;
+
+            var _center, _icon, _iconWidth, _iconHeight, _iconBackground;
+            if (isEvent) {
+                var info = child.a('$info');
+                _center = info.center || center;
+                _icon = info.icon || icon;
+                _iconWidth = info.iconWidth || iconWidth;
+                _iconHeight = info.iconHeight || iconHeight;
+                _iconBackground = info.iconBackground || iconBackground;
+
+                var func = function() {
+                    var x = (lineAreaWidth - _iconWidth) / 2, y = sy, width = _iconWidth, height = _iconHeight;
+                    if (_center) {
+                        y = sy + (preferredSize.height - _iconHeight) / 2;
+                    }
+                    ht.Default.drawImage(
+                        g,
+                        ht.Default.getImage(_icon),
+                        x,y,width,height,
+                        null, null, _iconBackground
+                    );
+                };
+                funcArr.push(func);
+            }
+
+            if (index === 0) {
+                if (isEvent) {
+                    if (_center) {
+                        g.moveTo(lineAreaWidth / 2, preferredSize.height / 2);
+                    }
+                    else {
+                        g.moveTo(lineAreaWidth / 2, _iconHeight / 2);
+                    }
+                }
+                else {
+                    g.moveTo(lineAreaWidth / 2, marginTop + preferredSize.height / 2);
+                }
+            }
+            else if (index === children.size() - 1) {
+                if (isEvent) {
+                    if (_center) {
+                        g.lineTo(lineAreaWidth / 2, sy + preferredSize.height / 2);
+                    }
+                    else {
+                        g.lineTo(lineAreaWidth / 2, sy + _iconHeight / 2);
+                    }
+                }
+                else {
+                    g.lineTo(lineAreaWidth / 2, sy + marginTop + preferredSize.height / 2);
+                }
+            }
+            funcArr.push(function() {
+                self.layoutChild(child, (isEvent ? lineAreaWidth : 0) + marginLeft + iconContentGap, sy + marginTop, preferredSize.width, preferredSize.height);
+            });
+            layoutY = sy;
+            if (isEvent) {
+                layoutY += Math.max(_iconHeight, marginTop + preferredSize.height + marginBottom);
+            }
+            else layoutY += marginTop + preferredSize.height + marginBottom;
+        });
+        g.lineWidth = lineWidth;
+        g.strokeStyle = lineColor;
+        g.stroke();
+
+        funcArr.forEach(function(func) {
+            func();
+        });
+        self.updateScrollBar(width, height, scrollWidth, layoutY);
+
+        g.restore();
+    },
+
+    _getLineAreaWidth: function() {
+        var self = this,
+            children = self.getVisibleChildren(),
+            iconWidth = self.getIconWidth(),
+            width = iconWidth;
+
+        children.each(function(child) {
+            var type = child.a('$type');
+
+            if (type === 'event') {
+                var info = child.a('$info');
+
+                width = Math.max(width, info.iconWidth || 0);
+            }
+        });
+
+        return width;
     },
 
     /**
@@ -58,6 +241,7 @@ def('ht.ui.TimeLine', ht.ui.VBoxLayout, {
         if (!time) return;
         var label = new ht.ui.Label();
 
+        label.a('$type', 'label');
         label.setText(time);
         label.setTextFont(font || this.getLabelFont());
         label.setTextColor(color || this.getLabelColor());
@@ -66,8 +250,7 @@ def('ht.ui.TimeLine', ht.ui.VBoxLayout, {
 
         this.addView(label, {
             width: 'wrap_content',
-            height: 'wrap_content',
-            marginBottom: 15
+            height: 'wrap_content'
         }, index);
     },
 
@@ -89,17 +272,18 @@ def('ht.ui.TimeLine', ht.ui.VBoxLayout, {
      *      contentBackground // 事件内容背景色
      *      border // 事件边框
      *      background // 事件背景
+     *      placement // header 位置，默认在上
+     *      center // 是否居中，默认不居中
      * }
      * @param index 插入到时间轴中的位置
      */
-
     addTimeEvent: function(event, index) {
         if (!event) return;
 
         var self = this,
             icon = event.icon,
-            iconWidth = event.iconWidth || 12,
-            iconHeight = event.iconHeight || 12,
+            iconWidth = event.iconWidth || this.getIconWidth(),
+            iconHeight = event.iconHeight || this.getIconHeight(),
             iconBackground = event.iconBackground || self.getIconBackground(),
             header = event.header,
             headerFont = event.headerFont || self.getHeaderFont(),
@@ -110,19 +294,25 @@ def('ht.ui.TimeLine', ht.ui.VBoxLayout, {
             contentColor = event.contentColor || self.getContentColor(),
             contentBackground = event.contentBackground,
             border = event.border || self.getEventBorder(),
-            background = event.background || self.getEventBackground();
+            background = event.background || self.getEventBackground(),
+            placement = event.placement || self.getPlacement(),
+            center = event.center || self.isCenter();
 
         var hbox = new ht.ui.HBoxLayout(),
             iconLabel = new ht.ui.Label(),
             vbox = new ht.ui.VBoxLayout();
+        
+        hbox.a('$type', 'event');
+        hbox.a('$info', event);
+        vbox.setGap(5);
 
         // 绘制时间轴上的图标
         iconLabel.setIcon(icon);
         iconLabel.setAlign('center');
         iconLabel.setVAlign('middle');
         iconLabel.setIconWidth(iconWidth);
-        iconLabel.setIconHeight(iconHeight),
-        iconLabel.setPreferredSize(iconWidth + 5, iconHeight + 5);
+        iconLabel.setIconHeight(iconHeight);
+        iconLabel.setPreferredSize(iconWidth, iconHeight);
         iconLabel.drawBackground = function() {
             var g = this.getRootContext(),
                 x = this.getContentWidth() / 2,
@@ -135,11 +325,10 @@ def('ht.ui.TimeLine', ht.ui.VBoxLayout, {
             g.fill();
         }
         
-        hbox.addView(iconLabel, {
-            width: 'wrap_content',
-            height: 'wrap_content',
-            marginLeft: self.getPaddingLeft() + self.getLineWidth() / 2 - (iconWidth + 5) / 2
-        });
+        // hbox.addView(iconLabel, {
+        //     width: 'wrap_content',
+        //     height: 'wrap_content'
+        // });
 
         // 绘制事件
         var headerView, contentView;
@@ -180,33 +369,28 @@ def('ht.ui.TimeLine', ht.ui.VBoxLayout, {
             
             vbox.addView(contentView, {
                 width: 'match_parent',
-                height: 'wrap_content',
-                marginTop: 5
-            });
+                height: 'wrap_content'
+            }, placement === 'bottom' ? 0 : undefined);
         }
         else if (content) {
             contentView = content;
             
             vbox.addView(contentView, {
                 width: 'match_parent',
-                height: 'wrap_content',
-                marginTop: 5
-            });
+                height: 'wrap_content'
+            }, placement === 'bottom' ? 0 : undefined);
         }
 
         vbox.setBorder(border);
         vbox.setBackground(background);
-        vbox.setPadding(10);
         hbox.addView(vbox, {
             width: 'match_parent',
-            height: 'wrap_content',
-            marginLeft: 20
+            height: 'wrap_content'
         });
 
         self.addView(hbox, {
             width: 'wrap_content',
-            height: 'wrap_content',
-            marginBottom: 20
+            height: 'wrap_content'
         }, index);
     },
     
@@ -246,7 +430,9 @@ def('ht.ui.TimeLine', ht.ui.VBoxLayout, {
             contentFont: true, 
             contentColor: true, 
             eventBorder: true,
-            eventBackground: true
+            eventBackground: true,
+            iconWidth: true,
+            iconHeight: true
         });
     }   
 });
